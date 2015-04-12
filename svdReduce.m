0508d1fc0edf8bbf511e44b0757bb97a6e2624ab
mmoscat <misael.moscat@gmail.com>
2015-04-12 18:56:11 -0400

    SVD Reduction using PCA algorithm

--- svdReduce.m
+++ svdReduce.m
@@ -23,27 +23,27 @@ function [recom_svd, Y_svd] = svdReduce(recom_matrix, Y_mean)
 recom_svd = recom_matrix;
 Y_svd = Y_mean;
 
-[U, S, V] = svd(recom_matrix, 1);
+m = size(recom_matrix, 2);
 
-D = diag(S);
-energy = sum(D .^ 2);
-current = energy;
+%% mean normalization
+rm_mean = mean(recom_matrix);
+rm_std = std(recom_matrix);
+rm_norm = recom_matrix;
 
-for i = 1:(size(D, 1));
-    [val index] = min(D);
-
-    current = current - (D(index) .^ 2);
-
-    D(index) = Inf;
-    S(index, :) = 0;
-
-    if ((current / energy)  < 0.9)
-        break;
-    endif
+for i = 1:m
+    rm_norm(:, i) = (recom_matrix(:, i) - rm_mean(i)) / rm_std(i);
 endfor
 
-recom_svd = U * S * V';
+%% using cov to calculate covariance matrix
+cov_mat = (rm_norm' * rm_norm) / m;
 
-printf("SVD Energy: %d%%\n", (current / energy) * 100);
+%% svd of covariance matrix
+[U, S, V] = svd(cov_mat);
+
+%% Take 90% of the dimensions
+new_U = U(:, 1:uint32(size(U, 1) * 0.90));
+
+%% Apply reduction:
+recom_matrix = recom_matrix * new_U;
 
 end
