ea07639dbf5ad5411f2c39de6f04182c5ab8b342
Anthony Salani <asalani93@gmail.com>
2015-04-25 12:59:14 -0400

    starts work on tags

--- data/recomp.py
+++ data/recomp.py
@@ -46,12 +46,40 @@ def loadUsers(users, files):
       })
 
 def convertToTagRatings(movies, tags, users):
+  for user in users:
+    totals = {}
+    for movieIdx, rating in user['ratings']:
+      for idx in movies[movieIdx]['tags']:
+        if idx in totals:
+          total, count = totals[idx]
+          totals[idx] = (total + rating, count + 1)
+        else:
+          totals[idx] = (rating, 1)
+    user['ratings'] = [(idx, totals[idx][0] / totals[idx][1]) for idx in totals.keys()]
+    print(user['id'])
+
+def filterTagsByCount(movies, tags, users, minHits):
+  oldTagCount = len(tags)
+  tags[:] = [tag for tag in tags if tag['count'] > minHits]
+
+  tagMap = {}
+  reIndex(tags, tagMap)
+
+  removedTagInstances = 0
+  for movie in movies:
+    oldMovieTags = len(movie['tags'])
+    movie['tags'] = [tagMap[idx] for idx in movie['tags'] if idx in tagMap]
+    removedTagInstances += oldMovieTags - len(movie['tags'])
+
+  return {
+    'removedTags': oldTagCount - len(tags),
+    'removedTagInstances': removedTagInstances
+  }
+
+def filterTagsByRatings(movies, tags, users):
   1
 
-def filterTagsByRatings(tags, users):
-  1
-
-def filterMoviesByTags(movies, tags, minMovies):
+def filterTaglessMovies(movies, tags, users):
   1
 
 def filterMoviesByRatings(movies, users):
@@ -94,7 +122,7 @@ if __name__ == '__main__':
   subparsers = parser.add_subparsers(dest = 'output')
   subparsers.required = True
   parserTags = subparsers.add_parser('tags')
-  parserTags.add_argument('-t', '--tagcount', default = 2000, type = int)
+  parserTags.add_argument('-c', '--count', default = 1400, type = int)
   parserMovies = subparsers.add_parser('movies')
   args = parser.parse_args()
 
@@ -105,10 +133,21 @@ if __name__ == '__main__':
     loadMovies(movies, inMovies)
     loadTags(tags, inTags)
     loadUsers(users, inUsers)
+    print(filterTagsByCount(movies, tags, users, args.count))
     convertToTagRatings(movies, tags, users)
-    filterTagsByCount(movies, tags, users, args.tagcount)
-    fitlerTagsByRatings(movies, tags, users)
-    filterTaglessMovies(movies, tags, users)
+
+    f = open('dump.csv', 'w')
+    print('Minimum,Maximum,Average', file = f)
+    for user in users:
+      r = list(zip(*user['ratings']))[1]
+      mn = min(r)
+      mx = max(r)
+      av = sum(r) / len(r)
+      print('{},{},{}'.format(mn, mx, av), file = f)
+      print('{}, {}, {}'.format(mn, mx, av))
+
+    #fitlerTagsByRatings(movies, tags, users)
+    #filterTaglessMovies(movies, tags, users)
   elif args.output == 'movies':
     loadMovies(movies, inMovies)
     loadUsers(users, inUsers)
